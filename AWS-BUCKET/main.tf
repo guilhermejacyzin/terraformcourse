ebf79b433674118efee056b71bd6d29d9ab2bb7b
Guilherme Jacyzin <jacyzin@gmail.com>
2025-06-29 04:18:23 -0300

    Adiciona configuração do bucket AWS e atualiza inicial.tf

---
+++ AWS-BUCKET/main.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-2"
+
+  default_tags {
+    tags = {
+        owner = "guilherme"
+        managed-by = "terraform"
+    }
+  }
+}
--- inicial.tf
+++ inicial.tf
@@ -1,12 +1,13 @@
 terraform {
-
-
+# para escolher a versão
+required_version = "1.3.0"
+# Exemple required_version >= "1.0.0" maior que 1.0.0
 }
 
 provider "aws" {
-
+# teste
 }
 
 resource "aws_instance" "name" {
-  
+# teste
 }
